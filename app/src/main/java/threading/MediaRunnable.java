8298f810490dcbdc31b2953f0c64343bd6ef7902
djohoe28 <djohoe28@gmail.com>
2024-08-30 15:58:18 +0300

    MediaRunnable moved MediaPlayer instantiation to `run()`

--- app/src/main/java/threading/MediaRunnable.java
+++ app/src/main/java/threading/MediaRunnable.java
@@ -6,6 +6,9 @@ import android.net.Uri;
 import android.os.Looper;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import java.util.Objects;
 
 public class MediaRunnable implements Runnable {
@@ -13,28 +16,45 @@ public class MediaRunnable implements Runnable {
     // Static
     private static final String TAG = "MediaRunnable";
     // Instance
-    private final MediaPlayer player;
+    // TODO: These values are used *only* during initialization, under `run()`
+    private final Context context;
+    private final int resId; // NOTE: Null mutex with `uri`
+    private final @Nullable Uri uri; // NOTE: Null mutex with `resId`
+    private final int volume;
+    private final boolean isMuted;
+
+    private MediaPlayer player;
     private MediaHandler handler;
     //#endregion
 
     //#region Constructors
-    public MediaRunnable(Context _context, int resId, int volume, boolean isMuted) {
+    public MediaRunnable(Context _context, int _resId, int _volume, boolean _isMuted) {
         Log.v(TAG, String.format("Constructor(resId=%d, volume=%d, isMuted=%s)",
-                resId, volume, isMuted));
-        player = MediaPlayer.create(_context, resId);
-        initializePlayer(volume, isMuted);
+                _resId, _volume, _isMuted));
+        context = _context;
+        resId = _resId;
+        uri = null;
+        volume = _volume;
+        isMuted = _isMuted;
     }
 
-    public MediaRunnable(Context _context, Uri uri, int volume, boolean isMuted) {
+    public MediaRunnable(Context _context, @NonNull Uri _uri, int _volume, boolean _isMuted) {
         Log.v(TAG, String.format("Constructor(uri=%s, volume=%d, isMuted=%s)",
-                uri, volume, isMuted));
-        player = MediaPlayer.create(_context, uri);
-        initializePlayer(volume, isMuted);
+                _uri, _volume, _isMuted));
+        context = _context;
+        resId = -1;
+        uri = _uri;
+        volume = _volume;
+        isMuted = _isMuted;
     }
     //#endregion
 
     //#region Methods
-    private void initializePlayer(int volume, boolean isMuted) {
+    private void initializePlayer() {
+        if (uri != null)
+            player = MediaPlayer.create(context, uri);
+        else
+            player = MediaPlayer.create(context, resId);
         player.setLooping(true);
         setVolume(volume);
         setIsMuted(isMuted);
@@ -44,6 +64,7 @@ public class MediaRunnable implements Runnable {
     //#region Overrides
     @Override
     public void run() {
+        initializePlayer();
         Looper.prepare();
         handler = new MediaHandler(Objects.requireNonNull(Looper.myLooper()), this);
         Looper.loop();
@@ -64,6 +85,7 @@ public class MediaRunnable implements Runnable {
     }
 
     public void setIsMuted(boolean value) {
+        if (player == null) return;
         // NOTE: Technically pausing rather than muting, but I think it's cute.
         if (value) player.pause();
         else player.start();
